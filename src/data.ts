65a5134a3b6f74cedadbf6abc51b7f9c9f5b58dd
Sidx005 <desaisiddharth1246@gmail.com>
2026-05-14 17:57:31 +0530

    Commented duplicated task

--- src/data.ts
+++ src/data.ts
@@ -4,6 +4,6 @@ export const initialTasks:Task[]=[
     {id:1,title:"Setup project",status:"done"},
     {id:2,title:"Install dependencies",status:"inprogress"},
     {id:3,title:"Deleting dependencies",status:"todo"},
-    {id:3,title:"Deleting dependencies",status:"todo"},
+    // {id:3,title:"Deleting dependencies",status:"todo"},
     {id:4,title:"Updated dependency",status:"done"},
 ]
