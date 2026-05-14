0a8a4f66a743706c716d932e22a7bc32390de360
Sidx005 <desaisiddharth1246@gmail.com>
2026-05-14 12:37:17 +0530

    Added one more task

--- src/data.ts
+++ src/data.ts
@@ -3,4 +3,5 @@ import type { Task } from "./types";
 export const initialTasks:Task[]=[
     {id:1,title:"Setup project",status:"done"},
     {id:2,title:"Install dependencies",status:"inprogress"},
+    {id:3,title:"Deleting dependencies",status:"todo"},
 ]
